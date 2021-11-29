c2e073f26436a622e68f172fbb58111f079afee1
Fraser Dillon <fraserdillon050388@gmail.com>
2021-11-29 09:13:48 +0000

    Final MVP Commit

--- classes/room.py
+++ classes/room.py
@@ -1,6 +1,14 @@
 class Room:
-    def __init__(self, name):
+    def __init__(self, name, guests, songs):
         self.name = name
-        self.customers = customers
+        self.guests = ()
         self.songs = songs
         
+    def guest_count(self):
+        return len(self.guests)
+
+    def add_guest(self, guest):
+        self.guests.append(guest)
+
+    def remove_guest(self):
+        return self.guests.pop()
--- tests/room_test.py
+++ tests/room_test.py
@@ -1,5 +1,4 @@
-import unittestimport unittest
-
+import unittest
 from classes.room import Room
 from classes.guest import Guest
 from classes.song import Song
@@ -7,13 +6,31 @@ from classes.song import Song
 class TestRoom(unittest.TestCase):
 
     def setUp(self):
-        self.customer_1 = Customer("Mr Red", 2.00, True)
-        self.customer_2 = Customer("Mr Blue", 3.00, True)
-        self.customer_3 = Customer("Mr Pink", 4.00, True)
-        self.customers = [self.customer_1, self.customer_2, self.customer_3]
+        self.guest_1 = Guest("Mr Red")
+        self.guest_2 = Guest("Mr Blue")
+        self.guest_3 = Guest("Mr Pink")
+        self.guests = [self.guest_1, self.guest_2, self.guest_3]
         self.song_1 = Song("Gotta Get")
         self.song_2 = Song("Through")
-        self.song_1 = Song("This")
+        self.song_3 = Song("This")
         self.songs = [self.song_1, self.song_2, self.song_3]
-        self.room = Room("Happening Room", 100.00, self.drinks)
+        self.room = Room("Happening Room",(),())
+
+    def test_room_has_guests(self):
+        self.assertEqual(3, self.room.guest_count())
+
+    def test_room_can_check_in_guests(self):
+        guest_4 = Guests("Mr Yellow")
+        self.room.add_guest(guest_4)
+        self.assertEqual(4, self.room.guest_count())
+
+    def test_room_can_check_out_guests(self):
+        self.room.remove_guest()
+        self.assertEqual(3, self.room.guest_count())
+
+    
+
+    
+
+    
         
